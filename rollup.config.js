98fdc2f30868ec0e9057c1cb8dc066b09cddbff1
Marton Lederer <martondeveloper@gmail.com>
2020-07-25 09:59:46 +0200

    Cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -1,77 +1,53 @@
-import svelte from 'rollup-plugin-svelte'
-import resolve from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
-import livereload from 'rollup-plugin-livereload'
-import { terser } from 'rollup-plugin-terser'
-import sveltePreprocess, { sass } from 'svelte-preprocess'
-import typescript from '@rollup/plugin-typescript'
-import image from '@rollup/plugin-image'
+import svelte from "rollup-plugin-svelte";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import livereload from "rollup-plugin-livereload";
+import { terser } from "rollup-plugin-terser";
+import sveltePreprocess, { sass } from "svelte-preprocess";
+import typescript from "@rollup/plugin-typescript";
+import image from "@rollup/plugin-image";
 
-const production = !process.env.ROLLUP_WATCH
+const production = !process.env.ROLLUP_WATCH;
 
 export default {
+  input: "src/main.ts",
+  output: {
+    sourcemap: true,
+    format: "iife",
+    name: "app",
+    file: "public/build/bundle.js"
+  },
+  plugins: [
+    svelte({
+      dev: !production,
+      css: css => css.write("public/build/bundle.css"),
+      preprocess: sveltePreprocess()
+    }),
+    resolve({ browser: true, dedupe: ["svelte"] }),
+    commonjs(),
+    typescript({ sourceMap: !production }),
+    sass(),
+    image(),
 
-	input: 'src/main.ts',
-	output: {
-
-		sourcemap: true,
-		format: 'iife',
-		name: 'app',
-		file: 'public/build/bundle.js'
-
-	},
-	plugins: [
-
-		svelte({
-
-			dev: !production,
-			css: css => css.write('public/build/bundle.css'),
-			preprocess: sveltePreprocess()
-
-		}),
-
-		resolve({ browser: true, dedupe: ['svelte'] }),
-		commonjs(),
-		typescript({ sourceMap: !production }),
-		sass(),
-		image(),
-
-		!production && serve(),
-		!production && livereload('public'),
-		production && terser()
-
-	],
-	watch: {
-
-		clearScreen: false
-
-	}
-
-}
+    !production && serve(),
+    !production && livereload("public"),
+    production && terser()
+  ],
+  watch: { clearScreen: false }
+};
 
 function serve() {
-
-	let started = false
+	let started = false;
 
 	return {
-
 		writeBundle() {
-
-			if (!started) {
-
-				started = true
-
-				require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-
-					stdio: ['ignore', 'inherit', 'inherit'],
+			if(!started) {
+				started = true;
+				require("child_process").spawn("npm", ["run", "start", "--", "--dev"], {
+					stdio: ["ignore", "inherit", "inherit"],
 					shell: true
-
-				})
-
+				});
 			}
-
 		}
-
 	}
-
 }
