77fe73e42fcf1c8471aec6c50fc443939182fc6f
Marton <martondeveloper@gmail.com>
2020-07-26 13:25:01 +0200

    Add 404 Not Found styles

--- src/views/NotFound.svelte
+++ src/views/NotFound.svelte
@@ -1,9 +1,51 @@
-<h1>404 Not Found</h1>
-<a href="/">Home</a>
+<script lang="ts">
+
+  import Button from "../components/Button.svelte";
+
+</script>
+
+<div class="NotFound">
+  <div class="content">
+    <h1><span>404</span> Not Found</h1>
+    <p>This is not the page you're looking for.</p>
+    <Button href="/" reverse={true}>Go home</Button>
+    <Button href="/docs" reverse={true} clear={true}>View docs</Button>
+  </div>
+</div>
 
 <style lang="sass">
 
-  h1
-    text-align: center
+  .NotFound
+    width: 100vw
+    height: 100vh
+    position: relative
+    background-color: #000
+
+    .content
+      position: absolute
+      top: 50%
+      left: 50%
+      transform: translate(-50%, -50%)
+      text-align: center
+
+      \:global(.Button)
+        margin-bottom: 10px
+
+      h1, p
+        color: #fff
+        text-align: center
+
+      h1
+        font-family: "Inter", sans-serif
+        font-weight: 600
+        font-size: 3em
+
+        @media screen and (max-width: 720px)
+          font-size: 2.5em
+
+        span
+          background: -webkit-linear-gradient(#9300B8, #C54DFD, #E1A1FF)
+          -webkit-background-clip: text
+          -webkit-text-fill-color: transparent
 
 </style>
