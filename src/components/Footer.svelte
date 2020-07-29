d51a30615edd2b2a31d8eeb6dd39da0189016f85
tate <tateflyer1@gmail.com>
2020-07-28 23:03:07 -0400

    feat: initialized a footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,40 @@
+<div class="Footer">
+    <div class="FooterContent">
+        <div>
+            <a href="/docs">docs</a>
+        </div>
+        <div>
+            <a href="https://github.com/t8/coinary-app">source code</a>
+        </div>
+        <div>
+            <a href="https://www.arweave.org/">arweave</a>
+        </div>
+    </div>
+</div>
+
+<style lang="sass">
+
+.Footer
+    width: 85%
+    border-top: 5px solid black
+    margin: 0 auto
+
+    .FooterContent
+        margin: 30px auto
+
+        div
+            width: 30%
+            display: inline-block
+
+        a
+            text-decoration: none
+            width: 100% !important
+            text-align: center !important
+            transition: color linear 0.1s
+            color: black
+        
+            &:hover
+                transition: color linear 0.1s
+                color: #C54DFD
+
+</style>
