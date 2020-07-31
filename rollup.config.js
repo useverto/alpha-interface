8b19194e3726693014c0afde0ab04a23284b479c
Marton <marton@lederer.hu>
2020-07-31 17:59:35 +0200

    Implement sapper base

--- rollup.config.js
+++ rollup.config.js
@@ -1,55 +1,114 @@
-import svelte from "rollup-plugin-svelte";
 import resolve from "@rollup/plugin-node-resolve";
+import replace from "@rollup/plugin-replace";
 import commonjs from "@rollup/plugin-commonjs";
-import livereload from "rollup-plugin-livereload";
+import svelte from "rollup-plugin-svelte";
+import babel from "@rollup/plugin-babel";
 import { terser } from "rollup-plugin-terser";
+import config from "sapper/config/rollup.js";
+import pkg from "./package.json";
 import sveltePreprocess, { sass } from "svelte-preprocess";
 import typescript from "@rollup/plugin-typescript";
 import image from "@rollup/plugin-image";
 import url from "@rollup/plugin-url";
 
-const production = !process.env.ROLLUP_WATCH;
+const mode = process.env.NODE_ENV;
+const dev = mode === "development";
+const legacy = !!process.env.SAPPER_LEGACY_BUILD;
+
+const onwarn = (warning, onwarn) => (warning.code === "CIRCULAR_DEPENDENCY" && /[/\\]@sapper[/\\]/.test(warning.message)) || onwarn(warning);
 
 export default {
-  input: "src/main.ts",
-  output: {
-    sourcemap: true,
-    format: "iife",
-    name: "app",
-    file: "public/build/bundle.js"
-  },
-  plugins: [
-    svelte({
-      dev: !production,
-      css: css => css.write("public/build/bundle.css"),
-      preprocess: sveltePreprocess()
-    }),
-    resolve({ browser: true, dedupe: ["svelte"] }),
-    commonjs(),
-    typescript({ sourceMap: !production }),
-    sass(),
-    image(),
-    url(),
+	client: {
+		input: config.client.input(),
+		output: config.client.output(),
+		plugins: [
+			replace({
+				"process.browser": true,
+				"process.env.NODE_ENV": JSON.stringify(mode)
+			}),
+			svelte({
+				dev,
+				hydratable: true,
+        emitCss: true,
+        css: css => css.write("public/build/bundle.css"),
+        preprocess: sveltePreprocess()
+			}),
+			resolve({
+				browser: true,
+				dedupe: ["svelte"]
+			}),
+      commonjs(),
+      sass(),
+      image(),
+      url(),
 
-    !production && serve(),
-    !production && livereload("public"),
-    production && terser()
-  ],
-  watch: { clearScreen: false }
+			legacy && babel({
+				extensions: [".js", ".mjs", ".html", ".svelte"],
+				babelHelpers: "runtime",
+				exclude: ["node_modules/@babel/**"],
+				presets: [
+					["@babel/preset-env", {
+						targets: "> 0.25%, not dead"
+					}]
+				],
+				plugins: [
+					"@babel/plugin-syntax-dynamic-import",
+					["@babel/plugin-transform-runtime", {
+						useESModules: true
+					}]
+				]
+			}),
+
+			!dev && terser({
+				module: true
+			})
+		],
+
+		preserveEntrySignatures: false,
+		onwarn,
+	},
+
+	server: {
+		input: config.server.input(),
+		output: config.server.output(),
+		plugins: [
+			replace({
+				"process.browser": false,
+				"process.env.NODE_ENV": JSON.stringify(mode)
+			}),
+			svelte({
+				generate: "ssr",
+        dev,
+        preprocess: sveltePreprocess()
+			}),
+			resolve({
+				dedupe: ["svelte"]
+			}),
+      commonjs(),
+      sass(),
+      image(),
+      url()
+		],
+		external: Object.keys(pkg.dependencies).concat(require("module").builtinModules),
+
+		preserveEntrySignatures: "strict",
+		onwarn,
+	},
+
+	serviceworker: {
+		input: config.serviceworker.input(),
+		output: config.serviceworker.output(),
+		plugins: [
+			resolve(),
+			replace({
+				"process.browser": true,
+				"process.env.NODE_ENV": JSON.stringify(mode)
+			}),
+			commonjs(),
+			!dev && terser()
+		],
+
+		preserveEntrySignatures: false,
+		onwarn,
+	}
 };
-
-function serve() {
-  let started = false;
-
-  return {
-    writeBundle() {
-      if (!started) {
-        started = true;
-        require("child_process").spawn("npm", ["run", "start", "--", "--dev"], {
-          stdio: ["ignore", "inherit", "inherit"],
-          shell: true
-        });
-      }
-    }
-  }
-}
---
+++ src/routes/index.svelte
@@ -0,0 +1,23 @@
+<script lang="typescript">
+
+  
+
+</script>
+
+<svelte:head>
+	<title>Sapper project template</title>
+</svelte:head>
+
+<h1>Great success!</h1>
+
+<figure>
+	<img alt="Success Kid" src="successkid.jpg">
+	<figcaption>Have fun with Sapper!</figcaption>
+</figure>
+
+<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+
+<style lang="sass">
+	h1
+    font-size: 4em
+</style>
