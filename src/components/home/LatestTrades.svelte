81176cc04b125a685f452f3b51b4022890176725
Marton <marton@lederer.hu>
2020-07-31 18:23:10 +0200

    Add original files

---
+++ src/components/home/LatestTrades.svelte
@@ -0,0 +1,100 @@
+<script lang="typescript">
+
+  import { fade } from "svelte/transition";
+  import { backOut } from "svelte/easing";
+
+  let element, y, windowHeight, shown = false;
+
+  $: {
+    if(element !== undefined) {
+      let componentY = element.offsetTop + element.offsetHeight;
+      if(componentY <= (y + windowHeight + 120) && !shown) shown = true;
+    }
+  }
+
+</script>
+
+<svelte:window bind:scrollY={y} bind:innerHeight={windowHeight} />
+<div class="LatestTrades" bind:this={element}>
+  {#if shown}
+    <div in:fade={{ duration: 1100, delay: 411, easing: backOut }}>
+      <h1 class="title">Latest Trades</h1>
+      <table>
+        <tr>
+          <th>TxID</th>
+          <th>AMOUNT</th>
+          <th>PST</th>
+        </tr>
+        <tr>
+          <td>jYKHLCGQuhQyt9uyZNXA6852CzYTu3qVYRKC6pnxIbkzDThbAgip</td>
+          <td>0.00007337</td>
+          <td class="pst">egg</td>
+        </tr>
+        <tr>
+          <td>cHZG6U7TzXYykn8m5g6s7vpYpbRvVpthUUpgCI4r9n8AXJKD5Gs1</td>
+          <td>0.00003450</td>
+          <td class="pst">wav</td>
+        </tr>
+        <tr>
+          <td>v5ty9DKrcb9Lnk3yJAdkSA9Eg5Lb6tSwr8rjJNS7Mou5eyGxRbnD</td>
+          <td>0.00000043</td>
+          <td class="pst">arc</td>
+        </tr>
+        <tr>
+          <td>DHy8qyXUJYA3Ygb9y7jujuvwP8eVr9MTpK8Kbl45zYIj3g5KdzgK</td>
+          <td>0.02300443</td>
+          <td class="pst">egg</td>
+        </tr>
+        <tr>
+          <td>vWwPCJWLbFhJ253u25zb3rvtJCB7TvPQ9cxvmQk0qICHLYKfnPgd</td>
+          <td>0.00000242</td>
+          <td class="pst">lum</td>
+        </tr>
+      </table>
+    </div>
+  {/if}
+</div>
+
+<style lang="sass">
+
+  .LatestTrades
+    padding: 1em 15vw 3em
+
+    @media screen and (max-width: 720px)
+      overflow-x: auto
+      margin-right: 15vw
+
+    h1.title
+      font-size: 2.3em
+      font-weight: 600
+
+      @media screen and (max-width: 720px)
+        width: 100%
+        font-size: 2.01em
+
+    table
+      width: 100%
+
+      tr
+        th, td
+          text-align: left
+
+        th
+          font-weight: 600
+          padding-bottom: 1.15em
+          text-decoration: underline
+
+        td
+          width: 20%
+          padding-bottom: .7em
+
+          &:first-child
+            width: 70%
+
+          &:last-child
+            width: 10%
+
+          &.pst
+            text-transform: uppercase
+
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,23 +1,24 @@
 <script lang="typescript">
 
-  
+  import NavBar from "../components/NavBar.svelte";
+  import Hero from "../components/home/Hero.svelte";
+  import LatestTrades from "../components/home/LatestTrades.svelte";
+  import About from "../components/home/About.svelte"
 
 </script>
 
 <svelte:head>
-	<title>Sapper project template</title>
+  <title>Coinary</title>
 </svelte:head>
 
-<h1>Great success!</h1>
-
-<figure>
-	<img alt="Success Kid" src="successkid.jpg">
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
-
-<style lang="sass">
-	h1
-    font-size: 4em
-</style>
+<NavBar blur={true} />
+<Hero />
+<LatestTrades />
+<About id="read-more">
+  <h1 class="title">What are PSTs?</h1>
+  <p class="about-description">Profit Sharing Tokens, or PSTs, are a new incentivization mechanism for the open web that allow developers to earn a stream of micro-dividends for the duration their application is used. (<a href="https://medium.com/@arweave/profit-sharing-tokens-a-new-incentivization-mechanism-for-an-open-web-1f2532411d6e">Source</a>)<br><br>In order for PSTs to have value, however, they need to be able to be exchanged for other PSTs or AR. This is where coinary comes in...</p>
+</About>
+<About>
+  <h1 class="title">Why Coinary?</h1>
+  <p class="about-description">Coinary is a completely decentralized network of trading posts built on top of the blockweave. Anyone can host their own trading post and power the exchange, while also being incentivized to do so. With coinary, you can pick the trading post you'd like to use and exchange your PSTs freely!<br><br>Decisions for coinary are made by our very own PST DAO, which means that anyone can have a say in the direction of our platform.</p>
+</About>
