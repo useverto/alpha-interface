4f9f013a3d289ebe32f5f9cdb4ac57df0b057ae8
tate <tateflyer1@gmail.com>
2020-07-31 22:12:40 -0400

    feat: footer

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,6 +4,7 @@
   import Hero from "../components/home/Hero.svelte";
   import LatestTrades from "../components/home/LatestTrades.svelte";
   import About from "../components/home/About.svelte";
+  import Footer from "../components/Footer.svelte";
 
 </script>
 
@@ -22,3 +23,4 @@
   <h1 class="title">Why Coinary?</h1>
   <p class="about-description">Coinary is a completely decentralized network of trading posts built on top of the blockweave. Anyone can host their own trading post and power the exchange, while also being incentivized to do so. With coinary, you can pick the trading post you'd like to use and exchange your PSTs freely!<br><br>Decisions for coinary are made by our very own PST DAO, which means that anyone can have a say in the direction of our platform.</p>
 </About>
+<Footer />
