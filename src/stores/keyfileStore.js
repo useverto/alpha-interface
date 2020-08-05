0c066a28396562db131ece932f09ea4b485aefe2
Marton <marton@lederer.hu>
2020-08-05 11:45:41 +0200

    Prototype login in keyfile store

--- src/stores/keyfileStore.js
+++ src/stores/keyfileStore.js
@@ -1,11 +1,13 @@
 import { writable, derived } from "svelte/store";
 import { userinfo } from "./userStore.js";
+// import Arweave from "arweave/web";
 
 // We store the arweave keyfile here.
 // It gets saved to the local stroage of the browser
 // It never leaves the user's browser
 
 export const keyfile = createKeyfileStore();
+// export const client = createArweaveClientStore();
 
 function createKeyfileStore () {
   const { subscribe, set } = writable("");
@@ -33,7 +35,7 @@ function createKeyfileStore () {
 export const loggedIn = derived(
   keyfile,
   $keyfile => ($keyfile !== "" && $keyfile !== "null" && $keyfile !== null && $keyfile !== undefined)
-)
+);
 
 // log out
 // this removes the keyfile from local stroage 
@@ -42,3 +44,28 @@ export function logOut () {
   keyfile.reset();
   userinfo.reset();
 }
+
+/*
+
+// the client store
+// used to connect to arweave
+function createArweaveClientStore () {
+  const { subscribe, set } = writable({});
+  if(process.browser) {
+    set(new Arweave({
+      host: "arweave.net",
+      port: 443,
+      protocol: "https",
+      timeout: 20000,
+    }));
+  }
+  return { set, subscribe };
+}
+
+export async function logIn (keyfile) {
+  if(!process.browser) return;
+  let address, balance;
+  await $client.wallets.jwkToAddress(JSON.parse(keyfile)).then(_address => address = _address);
+  await $client.wallets.getBalance(address).then(_balance => balance = $client.ar.winstonToAr(_balance));
+  console.log(address, balance);
+}*/
