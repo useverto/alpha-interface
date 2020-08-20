1b7b69485475d68ee3461c00231e3f7267bbaed3
John Letey <johnletey@gmail.com>
2020-08-20 11:57:25 +0100

    feat(wip): Add logic to the all exchanges page

--- src/routes/app/all-exchanges.svelte
+++ src/routes/app/all-exchanges.svelte
@@ -1,13 +1,53 @@
 <script lang="typescript">
 
+  import { loggedIn } from "../../stores/keyfileStore.js";
   import NavBar from "../../components/NavBar.svelte";
   import Footer from "../../components/Footer.svelte";
-  import moment from "moment";
-  import { loggedIn } from "../../stores/keyfileStore.js";
-  import { goto } from "@sapper/app";
   import { fade } from "svelte/transition";
 
+  import { query } from "../../api-client";
+  import moment from "moment";
+
   if(process.browser && !$loggedIn) goto("/");
+
+  let exchanges = getLatestExchanges();
+
+  async function getLatestExchanges (): Promise<{ timestamp: string, status: string }[]> {
+    if(!process.browser) return [];
+    
+    let exchanges: { timestamp: string, status: string }[] = [];
+    
+    // TODO(@johnletey): Figure out the other query tags
+    const txs = (await query(`
+      query {
+        transactions(
+          tags: [
+            {
+              name: "App-Name",
+              values: "Verto"
+            }
+          ]
+        ) {
+          edges {
+            node {
+              block {
+                timestamp
+              }
+            }
+          }
+        }
+      }
+    `)).data.transactions.edges;
+    
+    txs.map(({ node }) => {
+      exchanges.push({
+        timestamp: moment.unix(node.block.timestamp).format("YYYY-MM-DD hh:mm:ss"),
+        status: "pending"
+      })
+    })
+
+    return exchanges;
+  }
 
 </script>
 
@@ -22,68 +62,25 @@
     <tr>
       <th>Timestamp</th>
       <th>Exchange</th>
-      <th>Durration</th>
+      <th>Duration</th>
     </tr>
-    <tr>
-      <td style="width: 30%">{moment().format("YYYY-MM-DD hh:mm:ss")}</td>
-      <td style="width: 45%">0.00075664 <span class="currency">egg</span> {"->"} 0.00063480 <span class="currency">lum</span> <span class="status pending"></span></td>
-      <td style="text-transform: uppercase">4hrs 20min</td>
-    </tr>
-    <tr>
-      <td style="width: 30%">{moment().format("YYYY-MM-DD hh:mm:ss")}</td>
-      <td style="width: 45%">0.00075664 <span class="currency">egg</span> {"->"} 0.00063480 <span class="currency">lum</span> <span class="status success"></span></td>
-      <td style="text-transform: uppercase">4hrs 20min</td>
-    </tr>
-    <tr>
-      <td style="width: 30%">{moment().format("YYYY-MM-DD hh:mm:ss")}</td>
-      <td style="width: 45%">0.00075664 <span class="currency">egg</span> {"->"} 0.00063480 <span class="currency">lum</span> <span class="status failure"></span></td>
-      <td style="text-transform: uppercase">4hrs 20min</td>
-    </tr>
-    <tr>
-      <td style="width: 30%">{moment().format("YYYY-MM-DD hh:mm:ss")}</td>
-      <td style="width: 45%">0.00075664 <span class="currency">egg</span> {"->"} 0.00063480 <span class="currency">lum</span> <span class="status pending"></span></td>
-      <td style="text-transform: uppercase">4hrs 20min</td>
-    </tr>
-    <tr>
-      <td style="width: 30%">{moment().format("YYYY-MM-DD hh:mm:ss")}</td>
-      <td style="width: 45%">0.00075664 <span class="currency">egg</span> {"->"} 0.00063480 <span class="currency">lum</span> <span class="status pending"></span></td>
-      <td style="text-transform: uppercase">4hrs 20min</td>
-    </tr>
-    <tr>
-      <td style="width: 30%">{moment().format("YYYY-MM-DD hh:mm:ss")}</td>
-      <td style="width: 45%">0.00075664 <span class="currency">egg</span> {"->"} 0.00063480 <span class="currency">lum</span> <span class="status success"></span></td>
-      <td style="text-transform: uppercase">4hrs 20min</td>
-    </tr>
-    <tr>
-      <td style="width: 30%">{moment().format("YYYY-MM-DD hh:mm:ss")}</td>
-      <td style="width: 45%">0.00075664 <span class="currency">egg</span> {"->"} 0.00063480 <span class="currency">lum</span> <span class="status failure"></span></td>
-      <td style="text-transform: uppercase">4hrs 20min</td>
-    </tr>
-    <tr>
-      <td style="width: 30%">{moment().format("YYYY-MM-DD hh:mm:ss")}</td>
-      <td style="width: 45%">0.00075664 <span class="currency">egg</span> {"->"} 0.00063480 <span class="currency">lum</span> <span class="status pending"></span></td>
-      <td style="text-transform: uppercase">4hrs 20min</td>
-    </tr>
-    <tr>
-      <td style="width: 30%">{moment().format("YYYY-MM-DD hh:mm:ss")}</td>
-      <td style="width: 45%">0.00075664 <span class="currency">egg</span> {"->"} 0.00063480 <span class="currency">lum</span> <span class="status pending"></span></td>
-      <td style="text-transform: uppercase">4hrs 20min</td>
-    </tr>
-    <tr>
-      <td style="width: 30%">{moment().format("YYYY-MM-DD hh:mm:ss")}</td>
-      <td style="width: 45%">0.00075664 <span class="currency">egg</span> {"->"} 0.00063480 <span class="currency">lum</span> <span class="status success"></span></td>
-      <td style="text-transform: uppercase">4hrs 20min</td>
-    </tr>
-    <tr>
-      <td style="width: 30%">{moment().format("YYYY-MM-DD hh:mm:ss")}</td>
-      <td style="width: 45%">0.00075664 <span class="currency">egg</span> {"->"} 0.00063480 <span class="currency">lum</span> <span class="status failure"></span></td>
-      <td style="text-transform: uppercase">4hrs 20min</td>
-    </tr>
-    <tr>
-      <td style="width: 30%">{moment().format("YYYY-MM-DD hh:mm:ss")}</td>
-      <td style="width: 45%">0.00075664 <span class="currency">egg</span> {"->"} 0.00063480 <span class="currency">lum</span> <span class="status pending"></span></td>
-      <td style="text-transform: uppercase">4hrs 20min</td>
-    </tr>
+    {#await exchanges}
+      <!-- TODO: Loading state -->
+    {:then loadedExchanges}
+      {#if loadedExchanges.length === 0}
+        <p style="position: absolute; left: 50%; transform: translateX(-50%);">No exchanges found</p>
+        <tr><td><br></td><td></td></tr>
+      {/if}
+      {#each loadedExchanges as exchange}
+        <tr in:fade={{ duration: 300 }}>
+          <td style="width: 30%">{exchange.timestamp}</td>
+          <td style="width: 45%">0.00075664 <span class="currency">egg</span> {"->"} 0.00063480 <span class="currency">lum</span> <span class="status {exchange.status}"></span></td>
+          <td style="text-transform: uppercase">4hrs 20min</td>
+        </tr>
+        <tr>
+      </tr>
+      {/each}
+    {/await}
   </table>
 </div>
 <Footer />
