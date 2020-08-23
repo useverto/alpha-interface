260b58bada3923aac8a8079e0dfec38a082deb40
Marton Lederer <martondeveloper@gmail.com>
2020-08-23 21:42:25 +0200

    notification: merge old design with new

--- src/components/Notification.svelte
+++ src/components/Notification.svelte
@@ -2,9 +2,16 @@
 
   import { notification, NotificationType } from "../stores/notificationStore.js";
   import { cubicIn } from "svelte/easing";
-  import { fly, scale } from "svelte/transition";
+  import { fly, scale, fade } from "svelte/transition";
+  import errorIcon from "../assets/notification/error.svg";
+  import warningIcon from "../assets/notification/warning.svg";
+  import successIcon from "../assets/notification/success.svg";
+  import infoIcon from "../assets/notification/info.svg";
+  import closeIcon from "../assets/notification/close.svg";
 
-  let timeout = null;
+  let
+    timeout = null,
+    hover = false;
 
   $: notificationClass = $notification === null ? "" : Object.keys(NotificationType).find(key => NotificationType[key] === $notification.type);
 
@@ -20,9 +27,16 @@
 </script>
 
 {#if $notification !== null}
-  <div class="notification {notificationClass}" in:fly={{ x: 1000, opacity: 0, easing: cubicIn }} out:scale={{ duration: 200 }} on:click={hide}>
-    <h1>{$notification.title}</h1>
-    <p>{$notification.description}</p>
+  <div class="notification {notificationClass}" in:fly={{ x: 1000, opacity: 0, easing: cubicIn }} out:scale={{ duration: 200 }} on:click={hide} on:mouseover={() => hover = true} on:mouseleave={() => hover = false}>
+    {#if hover}
+      <img class="notification-icon" src={closeIcon} alt="close-notification" in:fade={{ duration: 300 }}>
+    {:else}
+      <img class="notification-icon" src={notificationClass === "error" ? errorIcon : (notification === "warning" ? warningIcon : (notificationClass === "success" ? successIcon : infoIcon))} alt={notificationClass}  in:fade={{ duration: 300 }}>
+    {/if}
+    <div class="notification-content">
+      <h1 class={notificationClass}>[{$notification.title}]</h1>
+      <p>{$notification.description}</p>
+    </div>
   </div>
 {/if}
 
@@ -32,46 +46,51 @@
     position: fixed
     bottom: 1.4em
     right: 1em
-    padding: .7em 1.5em
+    padding: .7em 1em
     background-color: rgba(#121212, .975)
     border-radius: 5px
     box-shadow: -2px 6px 17px 8px rgba(0, 0, 0, .18)
     z-index: 100000
     cursor: pointer
     overflow: hidden
-    border-left: 3px solid #fff
     max-width: 35vw
+    display: flex
+    align-items: center
     transition: all .3s
 
     @media screen and (max-width: 720px)
       max-width: 95vw
 
+    img.notification-icon
+      margin-right: .8em
+      height: 1.2em
+
     &:hover
       box-shadow: -2px 6px 17px 8px rgba(0, 0, 0, .26)
 
-    h1
-      color: #fff
-      font-weight: 500
-      font-size: 2em
-      font-family: "Inter", sans-serif
-      margin: 0
-        bottom: .2em
+    .notification-content
+      h1
+        margin: 0
+          bottom: .3em
+        font-size: 1.05em
+        font-weight: 400
 
-    &.error
-      border-color: #FF375D
+        &.error
+          color: #FF375D
 
-    &.warning
-      border-color: #FFD336
+        &.warning
+          color: #FFD336
 
-    &.success
-      border-color: #00D46E
+        &.success
+          color: #00D46E
 
-    &.log
-      border-color: #B075CD
+        &.log
+          color: #F2F2F2
 
-    p
-      color: #fff
-      font-size: 1.15em
-      margin: 0
+      p
+        color: #fff
+        font-size: 1.05em
+        margin: 0
+        font-weight: 400
 
 </style>
