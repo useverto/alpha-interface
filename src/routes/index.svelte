a464983895fd4b399e210a6b69793db6e31f4dec
tate <tateflyer1@gmail.com>
2020-08-24 23:09:22 -0400

    chore: Add link to trading post repo on index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -29,10 +29,17 @@
 <LatestTrades />
 <About id="read-more">
   <h1 class="title">What are PSTs?</h1>
-  <p class="about-description">Profit Sharing Tokens, or PSTs, are a new incentivization mechanism for the open web that allow developers to earn a stream of micro-dividends for the duration their application is used. (<a href="https://medium.com/@arweave/profit-sharing-tokens-a-new-incentivization-mechanism-for-an-open-web-1f2532411d6e">Source</a>)<br><br>In order for PSTs to have value, however, they need to be able to be exchanged for other PSTs or AR. This is where Verto comes in...</p>
+  <p class="about-description">
+    Profit Sharing Tokens, or PSTs, are a new incentivization mechanism for the open web that allow developers to earn a stream of micro-dividends for the duration their application is used. (<a href="https://medium.com/@arweave/profit-sharing-tokens-a-new-incentivization-mechanism-for-an-open-web-1f2532411d6e">Source</a>)<br><br>In order for PSTs to have value, however, they need to be able to be exchanged for other PSTs or AR. This is where Verto comes in...
+  </p>
 </About>
 <About>
   <h1 class="title">Why Verto?</h1>
-  <p class="about-description">Verto is a completely decentralized network of trading posts built on top of the blockweave. Anyone can host their own trading post and power the exchange, while also being incentivized to do so. With Verto, you can pick the trading post you'd like to use and exchange your PSTs freely!<br><br>Decisions for Verto are made by our very own PST DAO, which means that anyone can have a say in the direction of our platform.</p>
+  <p class="about-description">
+    Verto is a completely decentralized network of trading posts built on top of the blockweave. Anyone can host their own trading post and power the exchange, while also being incentivized to do so. With Verto, you can pick the trading post you'd like to use and exchange your PSTs freely!<br><br>Decisions for Verto are made by our very own PST DAO, which means that anyone can have a say in the direction of our platform.
+  </p>
+  <p class="about-desciption">
+    You can easily host your own Trading Post. See our <a href="https://github.com/useverto/trading-post">Trading Post Repository</a> for documentation.
+  </p>
 </About>
 <Footer />
