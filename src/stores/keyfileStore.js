a266bb74789c1c5bd7d7e4a7b558e2b0416ece63
divy-work <dj.srivastava23@gmail.com>
2020-09-03 14:47:10 +0530

    feat: format all supported files

--- src/stores/keyfileStore.js
+++ src/stores/keyfileStore.js
@@ -11,39 +11,50 @@ export const address = createCustomStore("address");
 
 // this is a custom store
 // it enables saving to local storage
-function createCustomStore (storeName) {
+function createCustomStore(storeName) {
   const { subscribe, set } = writable("");
 
-  if(process.browser && (localStorage.getItem(storeName) !== null && localStorage.getItem(storeName) !== "" && localStorage.getItem(storeName) !== "null" && localStorage.getItem(storeName) !== undefined)) { // is logged in according to the browser
-    set(localStorage.getItem(storeName))
+  if (
+    process.browser &&
+    localStorage.getItem(storeName) !== null &&
+    localStorage.getItem(storeName) !== "" &&
+    localStorage.getItem(storeName) !== "null" &&
+    localStorage.getItem(storeName) !== undefined
+  ) {
+    // is logged in according to the browser
+    set(localStorage.getItem(storeName));
   }
 
   return {
     subscribe,
-    reset: () => { // reset / log out
+    reset: () => {
+      // reset / log out
       set("");
       localStorage.removeItem(storeName);
     },
-    set: (val) => { // set
+    set: (val) => {
+      // set
       set(val);
       localStorage.setItem(storeName, val);
-    }
-  }
+    },
+  };
 }
 
 // return the balance
 export const balance = derived(
   address,
   ($address, set) => {
-    if(!process.browser) return;
-    const 
-      client = new Arweave({
+    if (!process.browser) return;
+    const client = new Arweave({
         host: "arweave.dev",
         port: 443,
         protocol: "https",
         timeout: 20000,
       }),
-      getBalance = () => client.wallets.getBalance($address).then(_balance => set(client.ar.winstonToAr(_balance)));
+      getBalance = () =>
+        client.wallets
+          .getBalance($address)
+          .then((_balance) => set(client.ar.winstonToAr(_balance)));
     getBalance();
     // refresh in every minute
     setInterval(getBalance, 60000);
@@ -56,12 +67,16 @@ export const balance = derived(
 // returns if the user is logged in
 export const loggedIn = derived(
   keyfile,
-  $keyfile => ($keyfile !== "" && $keyfile !== "null" && $keyfile !== null && $keyfile !== undefined)
+  ($keyfile) =>
+    $keyfile !== "" &&
+    $keyfile !== "null" &&
+    $keyfile !== null &&
+    $keyfile !== undefined
 );
 
 // log out
-// this removes the keyfile from local stroage 
-export function logOut () {
+// this removes the keyfile from local stroage
+export function logOut() {
   keyfile.reset();
 }
 
--- src/stores/notificationStore.js
+++ src/stores/notificationStore.js
@@ -2,23 +2,23 @@ import { writable } from "svelte/store";
 
 export const notification = createNotificationStore();
 
-function createNotificationStore () {
+function createNotificationStore() {
   const { set, subscribe } = writable({});
   set(null);
 
   return {
-    notify (title, description, type, timeout) {
-      if(typeof type !== "number" || type > 3) return;
+    notify(title, description, type, timeout) {
+      if (typeof type !== "number" || type > 3) return;
       set({ title, description, type, timeout });
     },
     subscribe,
-    remove: () => set(null)
-  }
+    remove: () => set(null),
+  };
 }
 
 export const NotificationType = {
   error: 0,
   warning: 1,
   log: 2,
-  success: 3
-}
+  success: 3,
+};
