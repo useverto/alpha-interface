8d73f07964d9ed6b1dd93f775ae10d1bbd55c081
tate <tateflyer1@gmail.com>
2020-09-08 22:58:17 -0400

    chore: update contract ID to new prod contract

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,7 @@
 // export const exchangeWallet = "pvPWBZ8A5HLpGSEfhEmK1A3PfMgB_an8vVS6L14Hsls";
 
 // For Production
-export const pstContract = "fE2OcfjlS-sHqG5K8QvxE8wHtcqKxS-YV0bDEgxo-eI";
+export const pstContract = "usjm4PCxUd5mtaon7zc97-dt-3qf67yPyqgzLnLqk5A";
 export const exchangeWallet = "aLemOhg9OGovn-0o4cOCbueiHT9VgdYnpJpq7NgMA1A";
 
 export const exchangeFee = 0.005;
