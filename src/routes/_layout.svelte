819280c659f178fd7e7a42d783b86342615826ea
tate <tateflyer1@gmail.com>
2020-09-10 01:43:03 -0400

    chore: fmt

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -4,10 +4,9 @@
   import { stores } from "@sapper/app";
 
   const ga_measurment_id = "UA-177614485-1";
-
 </script>
 
-<GoogleAnalytics {stores} id={ga_measurment_id}/>
+<GoogleAnalytics {stores} id={ga_measurment_id} />
 <Notification />
 <main>
   <slot />
