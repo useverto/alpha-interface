8d46f0f1fbd6c9623ee69e6fb9143348322d07da
John Letey <johnletey@gmail.com>
2020-09-13 11:50:27 +0100

    feat: Grab trading post addresses

---
+++ src/components/home/Volume.svelte
@@ -0,0 +1,51 @@
+<script lang="typescript">
+  import { query } from "../../api-client";
+  import { exchangeWallet } from "../../utils/constants";
+
+  const volume = getVolume();
+
+  async function getVolume() {
+    //
+
+    const gensisTxs = (
+      await query({
+        query: `
+      query($recipients: [String!]) {
+        transactions(
+          recipients: $recipients
+          tags: [
+            { name: "Exchange", values: "Verto" }
+            { name: "Type", values: "Genesis" }
+          ]
+        ) {
+          edges {
+            node {
+              owner {
+                address
+              }
+            }
+          }
+        }
+      }`,
+        variables: {
+          recipients: [exchangeWallet],
+        },
+      })
+    ).data.transactions.edges;
+
+    let posts: string[] = [];
+    gensisTxs.map((tx) => {
+      if (!posts.find((addr) => addr === tx.node.owner.address)) {
+        posts.push(tx.node.owner.address);
+      }
+    });
+
+    //
+
+    return [];
+  }
+</script>
+
+<div>
+  {#await volume}{/await}
+</div>
