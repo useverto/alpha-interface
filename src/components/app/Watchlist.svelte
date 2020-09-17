16e3be3ef7bafe65a0e98ae146a0e7add1efe03b
Marton Lederer <marton@lederer.hu>
2020-09-17 23:34:34 +0200

    watchlist: add remove button

--- src/components/app/Watchlist.svelte
+++ src/components/app/Watchlist.svelte
@@ -2,9 +2,16 @@
   import { watchlist } from "../../stores/watchlistStore.ts";
   import editIcon from "../../assets/edit.svg";
   import closeIcon from "../../assets/close.svg";
+  import addIcon from "../../assets/add.svg";
   import Line from "svelte-chartjs/src/Line.svelte";
+  import { fade } from "svelte/transition";
+  import type { IWatchlistElement } from "../../utils/types.ts";
 
   let editMode = false;
+
+  function add () {
+
+  }
 </script>
 
 <!-- prettier-ignore -->
@@ -44,11 +51,23 @@
 
         .graph-container
           width: 75%
-          display: 
+          border-radius: 3px
           transition: all .3s
 
           &.edit
+            position: relative
             cursor: pointer
+
+            img
+              position: absolute
+              top: -0.7em
+              right: -0.7em
+              width: 1em
+              height: 1em
+              padding: .2em
+              border-radius: 100%
+              background-color: #fff
+              box-shadow: 0 0 4px 4px rgba(0, 0, 0, .1)
 
             &:hover
               box-shadow: 0 0 9px 8px rgba(0, 0, 0, .1)
@@ -56,14 +75,27 @@
 </style>
 
 <div class="watchlist">
-  <h1 class="title">Watchlist <img src={editMode ? closeIcon : editIcon} alt="edit" on:click={() => { editMode = !editMode }}></h1>
+  <h1 class="title">
+    Watchlist
+    <div>
+      {#if editMode}
+        <img src={addIcon} alt="add" on:click={add} in:fade>
+        <img src={closeIcon} alt="close" on:click={() => { editMode = !editMode }} in:fade>
+      {:else}
+        <img src={editIcon} alt="edit" on:click={() => { editMode = !editMode }} in:fade>
+      {/if}
+    </div>
+  </h1>
   {#if $watchlist.length === 0}
     <p>No PSTs watched.</p>
   {:else}
     <div class="psts">
       {#each $watchlist as pst}
-        <div class="pst{editMode ? ' edit' : ''}">
-          <div class="graph-container">
+        <div class="pst" in:fade>
+          <div class="graph-container{editMode ? ' edit' : ''}">
+            {#if editMode}
+              <img src={closeIcon} alt="close" on:click={() => { watchlist.removePst(pst.pst) }} in:fade>
+            {/if}
             <Line
               data={{ labels: ['test', 'fd', 'fdfd', 'tgffgf'], datasets: [{ data: [10, 20 ,10 ,40, 60 ,80, 100, 61, 50, 70, 80, 60, 90], backgroundColor: 'transparent', borderColor: '#FF375D', pointBackgroundColor: '#FF375D' }] }}
               options={{ elements: { point: { radius: 0 } }, legend: { display: false }, scales: { xAxes: [{ ticks: { display: false }, gridLines: { display: false } }], yAxes: [{ ticks: { display: false }, scaleLabel: { display: false, fontFamily: '"JetBrainsMono", monospace', fontSize: 18 }, gridLines: { display: false } }] } }} />
