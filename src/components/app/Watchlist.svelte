1191e271eb00e6c6639ccba37348aee1d1f0085a
Marton Lederer <marton@lederer.hu>
2020-09-18 20:25:59 +0200

    watchlist: recode layout, fix some sass issues, separate functions, etc.

--- src/components/app/Watchlist.svelte
+++ src/components/app/Watchlist.svelte
@@ -4,18 +4,62 @@
   import closeIcon from "../../assets/close.svg";
   import addIcon from "../../assets/add.svg";
   import Line from "svelte-chartjs/src/Line.svelte";
-  import { fade } from "svelte/transition";
+  import { fade, scale } from "svelte/transition";
   import type { IWatchlistElement } from "../../utils/types.ts";
   import Modal from "../Modal.svelte";
+  import { notification } from "../../stores/notificationStore.ts";
+  import { NotificationType } from "../../utils/types.ts";
 
   let editMode = false;
   let addModalOpened = false;
-  let addPst: string = "test";
   let addPeriod: number = 24;
-  let addPsts: string[] = ["test", "fff", "xd"];
+  let psts: string[] = ["test", "mmmdfds", "fadff"];
+
+  $: addPsts = psts.filter(pst => $watchlist.filter((wEl: IWatchlistElement) => wEl.pst === pst).length === 0);
+  $: addPst = addPsts.length === 0 ? null : addPsts[0];
 
   function add() {
+    if(addPst === null) {
+      notification.notify(
+        "Error",
+        `No more PSTs available to add. Refresh the page!`,
+        NotificationType.error,
+        3000
+      );
+      return;
+    }
+
     watchlist.addPst({ pst: addPst, period: addPeriod });
+    addPeriod = 24;
+    notification.notify(
+      "Added",
+      `Added ${ addPst } to watchlist.`,
+      NotificationType.success,
+      2000
+    );
+  }
+
+  function remove (pst: IWatchlistElement) {
+    watchlist.removePst(pst.pst);
+    notification.notify(
+      "Removed",
+      `Removed ${ pst.pst } from watchlist.`,
+      NotificationType.error,
+      2000
+    );
+  }
+
+  function openAddModal () {
+    if(addPsts.length === 0) {
+      notification.notify(
+        "Error",
+        `No more PSTs available to add.`,
+        NotificationType.error,
+        1000
+      );
+      return;
+    }
+    addModalOpened = true;
   }
 </script>
 
@@ -23,7 +67,6 @@
 <style lang="sass">
 
   .watchlist
-    padding-bottom: 2.5em
 
     h1.title
       font-family: "Inter", sans-serif
@@ -45,18 +88,21 @@
       width: 100%
       display: flex
       flex-wrap: wrap
-      justify-content: space-between
 
       .pst
-        flex: 1 0 33%
         width: 33%
         height: 10em
         margin-bottom: 1em
         transition: all .3s
 
+        &:nth-child(3n)
+          order: 1
+
         .graph-container
-          width: 75%
+          width: calc(75% - .75em)
+          padding-right: .75em
           border-radius: 3px
+          margin: 0 auto
           transition: all .3s
 
           &.edit
@@ -88,7 +134,7 @@
           src={addIcon}
           alt="add"
           on:click={() => {
-            addModalOpened = true;
+            openAddModal();
           }}
           in:fade />
         <img
@@ -114,18 +160,21 @@
   {:else}
     <div class="psts">
       {#each $watchlist as pst}
-        <div class="pst" in:fade>
+        <div class="pst" in:fade={{ duration: 500 }} out:scale={{ duration: 250 }}>
           <div class="graph-container{editMode ? ' edit' : ''}">
             {#if editMode}
               <img
                 src={closeIcon}
                 alt="close"
                 on:click={() => {
-                  watchlist.removePst(pst.pst);
+                  remove(pst);
                 }}
                 in:fade />
             {/if}
             <!-- TODO @johnletey -->
+            <!-- Don't forget to change the color too :) -->
+            <!-- Color depends on increase or decrease (decrease: #FF375D, increase: #00D46E) -->
+            <!-- Make sure to change both the borderColor and the pointBackgroundColor, but not the backgroundColor -->
             <Line
               data={{ labels: ['test', 'fd', 'fdfd', 'tgffgf'], datasets: [{ data: [10, 20, 10, 40, 60, 80, 100, 61, 50, 70, 80, 60, 90], backgroundColor: 'transparent', borderColor: '#FF375D', pointBackgroundColor: '#FF375D' }] }}
               options={{ elements: { point: { radius: 0 } }, legend: { display: false }, scales: { xAxes: [{ ticks: { display: false }, gridLines: { display: false } }], yAxes: [{ ticks: { display: false }, scaleLabel: { display: false, fontFamily: '"JetBrainsMono", monospace', fontSize: 18 }, gridLines: { display: false } }] } }} />
@@ -136,17 +185,17 @@
   {/if}
 </div>
 <Modal bind:opened={addModalOpened} confirmation={true} onConfirm={add}>
-  <h1 style="text-align: center; font-weight: 400; margin-top: 0;">
+  <h1 style="text-align: center; font-weight: 600; margin-top: 0;">
     Add to watchlist
   </h1>
-  <h2 style="margin-bottom: 0; font-weight: 400;">PST</h2>
+  <h2 style="margin-bottom: 0; font-weight: 400; font-size: 1.24em;">PST</h2>
   <select
     bind:value={addPst}
     style="display: block; color: #fff; width: 100%; background-color: transparent; outline: none; border: 1px solid #fff; border-radius: 4px; font-size: 1.4em; padding: 0.18em 0.6em;">
     {#each addPsts as pst}
-      <option value={pst}>{pst}</option>
+      <option value={pst} style="background-color: #000;">{pst}</option>
     {/each}
   </select>
-  <h2 style="margin-bottom: 0; font-weight: 400;">Hours</h2>
+  <h2 style="margin-bottom: 0; font-weight: 400; font-size: 1.24em;">Period (hours)</h2>
   <input type="number" class="light" bind:value={addPeriod} />
 </Modal>
