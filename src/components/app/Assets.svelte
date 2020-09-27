57a470d43f51a5f5974f37c389ecb6d164fe8d18
John Letey <johnletey@gmail.com>
2020-09-27 12:44:24 +0100

    chore: remove assets graph

--- src/components/app/Assets.svelte
+++ src/components/app/Assets.svelte
@@ -1,65 +1,18 @@
 <script lang="ts">
   import Verto from "@verto/lib";
   import { address } from "../../stores/keyfileStore";
-  import Loading from "../../components/Loading.svelte";
   import SkeletonLoading from "../../components/SkeletonLoading.svelte";
-  import Pie from "svelte-chartjs/src/Pie.svelte";
 
   const client = new Verto();
   let balances: Promise<
     { id: string; ticker: string; balance: number }[]
   > = client.getAssets($address);
-  let options = {
-    responsive: true,
-  };
-  let noelements = false;
-  let balanceChart = populateChart();
-
-  async function populateChart() {
-    let data = await balances;
-    let chart = {
-      labels: [],
-      datasets: [
-        {
-          data: [],
-          backgroundColor: [
-            "#8548a3",
-            "#161616",
-            "#00D46E",
-            "#FFD336",
-            "#ff0000",
-            "#00d4b8",
-          ],
-          hoverBackgroundColor: [
-            "rgba(133,72,163,0.75)",
-            "rgba(22,22,22,0.75)",
-            "rgba(0,212,110,0.75)",
-            "rgba(255,211,54,0.75)",
-            "rgba(255,0,0,0.75)",
-            "rgba(0,212,184,0.75)",
-          ],
-        },
-      ],
-    };
-    for (let i = 0; i < data.length; i++) {
-      chart.labels.push(data[i].ticker);
-      chart.datasets[0].data.push(data[i].balance);
-    }
-
-    return chart;
-  }
-
-  function roundCurrency(val: number | string): string {
-    if (val === "?") return val;
-    if (typeof val === "string") val = parseFloat(val);
-    return val.toFixed(7);
-  }
 </script>
 
 <div class="section">
-  <div class="assets-table" class:noelements>
+  <div class="assets-table">
     <h1 class="title">Assets</h1>
-    <table style="{noelements ? 'position: relative' : ''}">
+    <table>
       <tr style="width: 100%">
         <th>Token</th>
         <th>Amount</th>
@@ -86,20 +39,13 @@
           <tr>
             <td>{balance.id}</td>
             <td>
-              {roundCurrency(balance.balance)}
+              {balance.balance}
               <span class="currency">{balance.ticker}</span>
             </td>
           </tr>
         {/each}
       {/await}
     </table>
-  </div>
-  <div class="assets-chart" class:noelements>
-    {#await balanceChart}
-      <Loading style="margin: 100px auto;" />
-    {:then data}
-      <Pie data="{data}" options="{options}" />
-    {/await}
   </div>
 </div>
 
@@ -123,11 +69,8 @@
       overflow-x: auto
 
     .assets-table
-      width: 50%
+      width: 100%
       transition: all .3s
-
-      &.noelements
-        width: 100%
 
       a.view-all
         display: block
@@ -143,12 +86,5 @@
       a.transaction
         text-decoration: none
         color: black
-
-    .assets-chart
-      width: 50%
-      transition: all .3s
-
-      &.noelements
-        width: 0%
 
 </style>
