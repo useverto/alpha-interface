e573e07d900e0fd865b7032cf619d116e1ce1d0f
John Letey <johnletey@gmail.com>
2020-11-21 21:54:23 +0000

    feat: add chat link to mobile footer

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -9,10 +9,10 @@
   } from "../stores/keyfileStore";
   import { notification } from "../stores/notificationStore";
   import { goto } from "@sapper/app";
-  import tradeLogo from "../assets/nav/trade.svg";
-  import tokensLogo from "../assets/nav/tokens.svg";
   import swapIcon from "../assets/nav/swap.svg";
-  import peopleLogo from "../assets/nav/people.svg";
+  import tokensIcon from "../assets/nav/tokens.svg";
+  import chatIcon from "../assets/nav/chat.svg";
+  import peopleIcon from "../assets/nav/people.svg";
   import { NotificationType, DisplayTheme } from "../utils/types";
   import { displayTheme } from "../stores/themeStore";
   import downArrow from "../assets/down-arrow.svg";
@@ -166,20 +166,20 @@
 {/if}
 <div class="mobile-nav" bind:clientHeight={mobileHeight}>
   {#if $loggedIn}
-    <a href="/trade"><object
-        data={tradeLogo}
-        type="image/svg+xml"
-        title="nav-icon" /></a>
     <a href="/swap"><object
         data={swapIcon}
+        type="image/svg+xml"
+        title="nav-icon" /></a>
+    <a href="/tokens"><object
+        data={tokensIcon}
         type="image/svg+xml"
         title="nav-icon" /></a>
     <a href="/app"><img
         class="verto"
         src={$displayTheme === DisplayTheme.Dark ? '/logo_dark.svg' : '/logo_light.svg'}
         alt="v" /></a>
-    <a href="/tokens"><object
-        data={tokensLogo}
+    <a href="/chat"><object
+        data={chatIcon}
         type="image/svg+xml"
         title="nav-icon" /></a>
     <a
@@ -187,7 +187,7 @@
       on:click={(e) => {
         e.preventDefault();
         showProfileSwitcher = !showProfileSwitcher;
-      }}><object data={peopleLogo} type="image/svg+xml" title="nav-icon" /></a>
+      }}><object data={peopleIcon} type="image/svg+xml" title="nav-icon" /></a>
   {/if}
 </div>
 
