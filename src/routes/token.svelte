e7cefe61e692213ccec7c558232f4e2db89ea1d9
tate <tateflyer1@gmail.com>
2020-12-20 13:31:42 -0500

    feat: graph and community description

--- src/routes/token.svelte
+++ src/routes/token.svelte
@@ -5,10 +5,14 @@
   import Verto from "@verto/lib";
   import { onMount } from "svelte";
   import { goto } from "@sapper/app";
+  import { fade } from "svelte/transition";
 
+  import Line from "svelte-chartjs/src/Line.svelte";
   import NavBar from "../components/NavBar.svelte";
   import Footer from "../components/Footer.svelte";
   import SkeletonLoading from "../components/SkeletonLoading.svelte";
+  import Loading from "../components/Loading.svelte";
+  import Button from "../components/Button.svelte";
 
   let client = new Verto();
   const arweaveClient = Arweave.init({
@@ -56,6 +60,54 @@
   }
 
   const communityLogo: Promise<string> = cLogo();
+
+  async function cTicker(): Promise<string> {
+    await communityName;
+    return state.ticker;
+  }
+
+  const communityTicker: Promise<string> = cTicker();
+
+  async function cGraph() {
+    await communityName;
+    const returnedPrices = await client.price(communityID);
+
+    let dates = returnedPrices.dates;
+    let prices = returnedPrices.prices;
+
+    const start = prices[0];
+    const end = prices[prices.length - 1];
+    let color;
+    const percentage = ((end - start) / start) * 100;
+    let percentageDisplay: string;
+    if (end >= start) {
+      color = "#00D46E";
+      percentageDisplay = "+" + percentage.toFixed(2) + "%";
+    } else {
+      color = "#FF375D";
+      percentageDisplay = percentage.toFixed(2) + "%";
+    }
+
+    return {
+      dates,
+      prices,
+      latestPrice: end.toFixed(4),
+      color,
+      percentage: percentageDisplay,
+    };
+  }
+
+  async function cDescription(): Promise<string> {
+    await communityName;
+    return (
+      state.settings.get("communityDescription") || "No Community Description"
+    );
+  }
+
+  async function cAppURL(): Promise<string> {
+    await communityName;
+    return state.settings.get("communityAppUrl") || "";
+  }
 </script>
 
 <svelte:head>
@@ -82,7 +134,56 @@
       {:then loadedName}
         {loadedName}
       {/await}
+      {#await communityTicker}
+        <SkeletonLoading />
+      {:then loadedTicker}
+        <span class="ticker">{loadedTicker}</span>
+      {/await}
     </h1>
+    <div class="cta">
+      {#await cAppURL()}
+        <SkeletonLoading />
+      {:then loadedURL}
+        <Button href={loadedURL}>Visit</Button>
+      {/await}
+    </div>
+  </div>
+  <div class="token-body">
+    <div class="graph-wrapper">
+      {#await cGraph()}
+        <div
+          style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
+          <Loading
+            style="width: 26px; height: 26px; animation-duration: .8s;" />
+        </div>
+      {:then loaded}
+        {#if loaded.prices.every((price) => isNaN(price))}
+          <p in:fade={{ duration: 130 }}>no data</p>
+        {:else}
+          <Line
+            data={{ labels: loaded.dates, datasets: [{ label: 'AR', data: loaded.prices, backgroundColor: '#ffe3ff', borderColor: function (context) {
+                    let gradient = context.chart.ctx.createLinearGradient(0, 0, context.chart.width, context.chart.height);
+                    gradient.addColorStop(0, '#E698E8');
+                    gradient.addColorStop(1, '#8D5FBC');
+                    return gradient;
+                  }, pointBackgroundColor: function (context) {
+                    let gradient = context.chart.ctx.createLinearGradient(0, 0, context.chart.width, context.chart.height);
+                    gradient.addColorStop(0, '#E698E8');
+                    gradient.addColorStop(1, '#8D5FBC');
+                    return gradient;
+                  } }] }}
+            options={{ elements: { point: { radius: 0 }, line: { borderWidth: 5, borderCapStyle: 'round' } }, tooltips: { mode: 'index', intersect: false }, hover: { mode: 'nearest', intersect: true }, maintainAspectRatio: false, legend: { display: false }, scales: { xAxes: [{ gridLines: { display: false }, scaleLabel: { display: false }, ticks: { display: false } }], yAxes: [{ gridLines: { display: false }, scaleLabel: { display: false }, ticks: { display: false } }] } }} />
+        {/if}
+      {/await}
+    </div>
+    <div class="description">
+      <h2>About</h2>
+      {#await cDescription()}
+        <SkeletonLoading />
+      {:then loadedDesc}
+        <p>{loadedDesc}</p>
+      {/await}
+    </div>
   </div>
 </div>
 
@@ -100,6 +201,8 @@
     .token-head
       display: flex
       margin-bottom: 2em
+      margin-top: 2em
+      position: relative
 
       @media screen and (max-width: 720px)
         display: block
@@ -110,13 +213,20 @@
         @media screen and (max-width: 720px)
           font-size: 1.35em
           margin-bottom: .3em
+        
+        .ticker
+          margin-left: 10px
+          color: rgba(17, 51, 83, 0.6)
+          text-transform: uppercase
+          font-size: 0.75em
+          font-family: "JetBrainsMono", monospace
 
       .logo
         width: 2.3em
         height: 2.3em
         border-radius: 100000px
         margin-top: 2px
-        margin-right: 15px
+        margin-right: 2em
 
         @media screen and (max-width: 720px)
           width: 1.35em
@@ -125,5 +235,14 @@
         img
           width: 100%
           height: 100%
+
+      .cta
+        position: absolute
+        right: 0
+        top: 0
+
+    .token-body
+      .graph-wrapper
+        margin-bottom: 3em
 
 </style>
